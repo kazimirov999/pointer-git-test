82b4f8572a45f1f33a62341982b5bcff1f448ee9
okazimirov <Hc147852>
2019-07-12 19:31:22 +0300

    improve performance

--- src/com/encoding/Main.java
+++ src/com/encoding/Main.java
@@ -4,13 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         int a = 7;
-        int b;
         String temp = "";
         while (a != 0) {
             b = a % 2;
             temp = b + temp;
             a = a / 2;
         }
-        System.out.print(temp);
+        System.out.println(temp);
     }
 }

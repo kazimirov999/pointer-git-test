05c9612b39fff91baee4156ff4c85f3d6e03902c
okazimirov <Hc147852>
2019-07-12 19:01:16 +0300

    init project

---
+++ src/com/encoding/Main.java
@@ -0,0 +1,20 @@
+package com.encoding;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        int a = -7;
+
+        int b;
+        String temp = "";
+        while (a != 0) {
+            b = a % 2;
+            temp = b + temp;
+            a = a / 2;
+        }
+        System.out.print(temp);
+    }
+}
